9fce1351055079ad20b62c9d3fb16b242b663651
NSFish <lxy910428@gmail.com>
2020-05-31 06:59:23 +0800

    dryrun 改为 NO

--- BangumiRenamer/main.m
+++ BangumiRenamer/main.m
@@ -101,7 +101,7 @@ int main(int argc, const char * argv[]) {
         [NSFBangumiRenamer renameFilesIn:destDirectoryURL
                               withSource:sourceFileURL
                                  pattern:patternFileURL
-                                  dryrun:YES];
+                                  dryrun:NO];
     }
     
     return 0;
